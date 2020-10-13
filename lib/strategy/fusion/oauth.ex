07d1edd5d761ab2fcfad3a6c40470e2664d5975d
elappas <evangelos.lappas@fantasysportsinteractive.co.uk>
2020-10-13 16:03:01 +0300

    Change error pattern matching when calling get_token

--- lib/strategy/fusion/oauth.ex
+++ lib/strategy/fusion/oauth.ex
@@ -77,9 +77,11 @@ defmodule Ueberauth.Strategy.Fusion.OAuth do
     case opts |> client |> OAuth2.Client.get_token(params) do
       {:error, %{body: %{"error" => error, "error_description" => description}}} ->
         {:error, {error, description}}
-      {:ok, %{token: %{access_token: nil} = token}} ->
-        %{"error" => error, "error_description" => description} = token.other_params
-        {:error, {error, description}}
+      {:error, %{body: _body, status_code: status_code} = _token} ->
+        {:error, {status_code, status_code}}
+      # {:ok, %{token: %{access_token: nil} = token}} ->
+      #   %{"error" => error, "error_description" => description} = token.other_params
+      #   {:error, {error, description}}
       {:ok, %{token: token}} ->
         {:ok, token}
     end
